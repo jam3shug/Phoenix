1c2889fedd9c783916e3042b24d54a7e5fffac3e
James Hughes <james@jxhug.me>
2023-09-04 13:31:10 -0700

    add fetchedGame object
    Signed-off-by: James Hughes <james@jxhug.me>

--- Phoenix/Phoenix/FetchGameData.swift
+++ Phoenix/Phoenix/FetchGameData.swift
@@ -12,10 +12,32 @@ struct FetchGameData {
     let wrapper: IGDBWrapper = IGDBWrapper(clientID: "aqxuk3zeqtcuquwswjrbohyi2mf5gc", accessToken: "go5xcl37bz41a16plvnudbe6a4fajt")
 
     func searchGameByName(name: String) {
+        if let idx = games.firstIndex(where: { $0.name == name }) {
+            let game = games[idx]
+            
+            var fetchedGame: Game = .init(
+                launcher: game.launcher,
+                metadata: [
+                    "description": game.metadata["description"] ?? "",
+                    "header_img": game.metadata["header_img"] ?? "",
+                    "rating": game.metadata["rating"] ?? "",
+                    "genre": game.metadata["genre"] ?? "",
+                    "developer": game.metadata["developer"] ?? "",
+                    "publisher": game.metadata["publisher"] ?? "",
+                    "release_date": game.metadata["release_date"] ?? "",
+                ],
+                icon: game.icon,
+                name: game.name,
+                platform: game.platform,
+                status: game.status,
+                is_deleted: game.is_deleted,
+                is_favorite: game.is_favorite
+            )
+        }
         // Create an APICalypse query to specify the search query and fields.
         if name != "" {
             let apicalypse = APICalypse()
-                .fields(fields: "id,name,artworks.image_id,artworks.height,category") // Specify the fields you want to retrieve
+                .fields(fields: "id,name,artworks.image_id,artworks.height,description") // Specify the fields you want to retrieve
                 .where(query: "name ~ *\"\(name)\"*") // Use the "where" clause to search by name
                 .limit(value: 50)
 
@@ -23,11 +45,7 @@ struct FetchGameData {
             wrapper.games(apiCalypse: apicalypse, result: { games in
                 // Handle the retrieved games here
                 if let lowestIdGame = games.min(by: { $0.id < $1.id }) {
-                    // Print the game with the lowest .id
-                    print(lowestIdGame.id)
-                    for i in 0..<lowestIdGame.artworks.count {
-                        print("https://images.igdb.com/igdb/image/upload/t_1080p_2x/\(lowestIdGame.artworks[i].imageID).jpg")
-                    }
+                    
                 }
             }) { error in
                 // Handle any errors that occur during the request
