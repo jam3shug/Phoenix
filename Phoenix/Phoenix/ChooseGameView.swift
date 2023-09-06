2bfcd8df05beeca1c3c1647fd4deb34c93b41ef9
James Hughes <james@jxhug.me>
2023-09-05 21:25:44 -0700

    create ChooseGameView
    Signed-off-by: James Hughes <james@jxhug.me>

---
+++ Phoenix/Phoenix/ChooseGameView.swift
@@ -0,0 +1,16 @@
+//
+//  ChooseGameView.swift
+//  Phoenix
+//
+//  Created by James Hughes on 9/5/23.
+//
+
+import SwiftUI
+
+struct ChooseGameView: View {
+    var body: some View {
+        List {
+            
+        }
+    }
+}
