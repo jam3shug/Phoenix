6d211e646445db1195943692cef378e8512e6e8a
James Hughes <james@jxhug.me>
2023-11-05 08:02:00 -0800

    appearance settings
    Signed-off-by: James Hughes <james@jxhug.me>

--- Phoenix/Phoenix/Views/Settings/AppearanceSettingsView.swift
+++ Phoenix/Phoenix/Views/Settings/AppearanceSettingsView.swift
@@ -6,35 +6,25 @@
 //
 
 import SwiftUI
-import AppKit
+import Defaults
+
+extension Defaults.Keys {
+    static let accentColorUI = Key<Bool>("accentColorUI", default: true)
+    static let listIconsHidden = Key<Bool>("listIconsHidden", default: false)
+    static let listIconSize = Key<Double>("listIconSize", default: 24.0)
+    static let showPickerText = Key<Bool>("showPickerText", default: false)
+    static let showSortByNumber = Key<Bool>("showSortByNumber", default: true)
+}
 
 struct AppearanceSettingsView: View {
-    
-    @AppStorage("accentColorUI")
-    private var accentColorUI: Bool = true
-    
-    @AppStorage("listIconsHidden")
-    private var listIconsHidden: Bool = false
-    
-    @AppStorage("listIconSize")
-    private var listIconSize: Double = 24
-    
-    @AppStorage("picker")
-    private var picker: Bool = true
-    
-    @AppStorage("sortByNumber")
-    private var sortByNumber: Bool = false
+    @Default(.listIconSize) var listIconSize
     
     var body: some View {
         Form {
             VStack(alignment: .leading, spacing: 20) {
-                Toggle(isOn: $accentColorUI) {
-                    Text("Adaptive Color UI")
-                }
-                Toggle(isOn: $listIconsHidden) {
-                    Text("Hide icons in sidebar")
-                }
-                if !listIconsHidden {
+                Defaults.Toggle("Adaptive Color UI", key: .accentColorUI)
+                Defaults.Toggle("Hide icons in sidebar", key: .listIconsHidden)
+                if !Defaults[.listIconsHidden] {
                     Slider(
                         value: $listIconSize,
                         in: 20...48,
@@ -48,12 +38,8 @@ struct AppearanceSettingsView: View {
                     }
                     .frame(maxWidth: 225)
                 }
-                Toggle(isOn: $picker) {
-                    Text("Show text in category picker")
-                }
-                Toggle(isOn: $sortByNumber) {
-                    Text("Show amount of games in sidebar")
-                }
+                Defaults.Toggle("Show text in category picker", key: .showPickerText)
+                Defaults.Toggle("Show amount of games in sidebar", key: .showSortByNumber)
             }
         }
     }
